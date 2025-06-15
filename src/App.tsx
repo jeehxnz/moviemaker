b394d64452152d3e2a432023ca45ed16d1242a5b
Jeehan Zaman <67488454+jeehxnz@users.noreply.github.com>
2025-06-15 19:42:29 -0400

    New project

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+
+const App = () => {
+  return ( 
+    <main > 
+      
+      <div className='pattern'>
+
+        <div className='wrapper'>
+          <header>
+            <img src="./hero.png" alt="Hero Banner"/>
+
+            <h1> Find <span className='text-gradient'> Movies</span> without Hassle</h1>
+          </header>
+
+          <p> Search </p>
+
+        </div>
+
+      </div>
+
+    </main>
+   );
+}
+ 
+export default App;
